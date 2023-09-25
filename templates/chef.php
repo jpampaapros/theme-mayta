cb556feb4b5a3e70ca5852b0d19437c4ec957565
JosCha94 <72150891+JosCha94@users.noreply.github.com>
2023-09-25 17:08:30 -0500

    cambios yachay view

--- templates/chef.php
+++ templates/chef.php
@@ -11,7 +11,7 @@ $texto = get_field("texto_descripcion");
 
 ?>
 <main class="container py-150">
-    <section class="sec-1 flex mx-65">
+    <section class="sec-1 flex mx-65 mb-250">
         <div class="w-50% relative">
             <img src="<?= home_url() ?>/wp-content\themes\theme-mayta\public\image\chef\Frame.png" class="w-783 h-952" alt="mayta">
             <img src="<?= $imagen['url'] ?>" class="w-705 h-975 absolute top-40 left-100" alt="<?= $imagen['alt'] ?>">
@@ -23,6 +23,9 @@ $texto = get_field("texto_descripcion");
             <div class="w-62 h-1 bg-white mb-79"></div>
             <div class="text-18 leading-27 text-white font-light text-chef"><?= $texto ?></div>
         </div>
+    </section>
+    <section>
+ <!-- <h1>TIME LINE</h1> -->
     </section>
 </main>
 
