94592fb7d4d467478b5087cc929a607dc0ad5ba2
JosCha94 <72150891+JosCha94@users.noreply.github.com>
2023-09-25 11:24:26 -0500

    cambios botones

--- templates/carta.php
+++ templates/carta.php
@@ -19,7 +19,7 @@ $link_carta = get_field("boton_link_carta");
 		<div class="flex flex-col absolute top-350 left-320">
 			<h2 class="font-medium text-50 text-white"> <?= $titulo ?></h2>
 			<div class="text-16 text-white"><?= $texto ?></div>
-			<a class="border border-white bg-transparent py-20 px-40 text-center h-auto w-250 text-18 font-medium my-20" href="<?= $link ?>"><?= $boton ?></a>
+			<a class="border border-white bg-transparent pt-20 pb-16 px-40 text-center h-auto w-250 text-18 font-medium my-20" href="<?= $link ?>"><?= $boton ?></a>
 		</div>
 		<img class="w-713 h-629 ml-220" src="<?= $imagen['url'] ?> " alt="<?= $imagen['alt'] ?>">
 	</section>
@@ -45,7 +45,7 @@ $link_carta = get_field("boton_link_carta");
 	<section class="flex justify-center">
 <div class="flex flex-col items-center justify-center py-30 text-white">
 <?= $carta ?>
-<a class="py-20 px-40 text-18 bg-transparent border border-white my-20" href="<?= $link_carta ?>"><?= $boton_carta ?></a>
+<a class="pt-20 pb-16 px-40 text-18 bg-transparent border border-white my-20" href="<?= $link_carta ?>"><?= $boton_carta ?></a>
 
 </div>
 	</section>
--- templates/mayta.php
+++ templates/mayta.php
@@ -111,11 +111,11 @@ $link_boton = get_field("boton_link_adicional");
 		<div class="w-auto h-auto relative border-8 border-grey">
 			<img src="<?= $imagen2['url'] ?>" class="w-1095 h-395" alt="<?= $imagen2['url'] ?>">
 			<div class="absolute top-30 w-full flex items-center flex-col">
-				<div class="text-50 bg-white text-black h-auto w-500 text-center font-bold mb-10">
-					<?= $titulo ?>
+				<div class=" bg-white pt-10  h-auto w-500 text-center  mb-10">
+					<h1 class="text-50 text-black font-bold"><?= $titulo ?></h1>
 				</div>
-				<h1 class="text-50"><?= $subtitulo ?></h1>
-				<a href="<?= $link_boton ?>" class="h-auto py-20 px-40 bg-gray text-18 font-medium border border-white">
+				<h2 class="text-50"><?= $subtitulo ?></h2>
+				<a href="<?= $link_boton ?>" class="h-auto pt-20 pb-16 px-40 bg-gray text-18 font-medium border border-white">
 					<?= $boton ?>
 				</a>
 			</div>
