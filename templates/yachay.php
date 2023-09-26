a200a4a95cf3b6ec02c8857550c97154c55ecdcc
JosCha94 <72150891+JosCha94@users.noreply.github.com>
2023-09-26 16:09:58 -0500

    cambios

--- templates/yachay.php
+++ templates/yachay.php
@@ -16,9 +16,9 @@ $link_boton = get_field("boton_link_adicional");
 $imagen2 = get_field("imagen_adicional");
 
 ?>
-<main class="container md:py-150 py-50">
-	<section class="flex flex-col mb-250">
-		<div class="flex flex-col md:items-center justify-center mb-56">
+<main class="md:py-150 py-50">
+	<section class="container flex flex-col md:mb-250 mb-90">
+		<div class="flex flex-col md:items-center justify-center md:mb-56 mb-30">
 			<div class="md:w-500 w-auto flex flex-col md:text-center text-left mb-60">
 				<h1 class="text-75 font-medium leading-90 text-white"><?= $titulo ?></h1>
 				<div class="h-1 md:w-60 w-110 bg-white md:ml-220"></div>
@@ -26,27 +26,34 @@ $imagen2 = get_field("imagen_adicional");
 			<!-- <img class="md:w-1514 md:h-533 w-358 h-237" src="<?= $imagen['url'] ?>" alt="<?= $imagen['alt'] ?>"> -->
 			<?= render_image($imagen,"md:w-1514 md:h-533 w-358 h-237") ?>
 		</div>
-		<div class="flex md:justify-between md:flex-row flex-col px-55">
-			<div class="flex flex-col md:w-500 w-auto">
+		<div class="flex md:justify-between md:flex-row flex-col md:px-55">
+			<div class="flex flex-col md:w-500 w-auto md:text-left text-right mb-30">
 				<h2 class="md:text-75 text-35 font-bold text-white"><?= $titulo ?></h2>
 				<h3 class="md:text-35 text-25 text-white"><?= $subtitulo ?></h3>
 			</div>
-			<div class="text-18 font-light w-500 flex items-center"><?= $texto ?></div>
+			<div class="md:text-18 text-16 font-light md:w-500 w-auto flex items-center"><?= $texto ?></div>
 		</div>
 	</section>
-	<section class="flex mb-250 gap-82">
+	<section class="container md:hidden block mb-90">
+		<div class="w-full ">
+		<p class="text-white font-600 text-16 leading-24">
+		Evolucionar gastronómicamente implica entender el origen de los insumos y su entorno, es por ello que constantemente imaginamos cómo podríamos trasladar la misma sensación a nuestros comensales conectándolos con nuestro territorio, tradición y cultura.”
+		</p>
+	</div>
+	</section>
+	<section class="md:container md:mx-0 mx-10 flex md:flex-row flex-col md:mb-250 mb-90 md:gap-82 gap-10">
 		<?php
 		$llave = 1;
 		foreach ($cards as $card) : ?>
 			<?php if ($llave % 2 !== 0) : ?>
-				<img class="w-347 h-589 mt-120" src="<?= $card['url'] ?>" alt="<?= $card['alt'] ?>">
+				<img class="md:w-347 md:h-589 w-171 h-276 md:mt-120 mt-30" src="<?= $card['url'] ?>" alt="<?= $card['alt'] ?>">
 			<?php else : ?>
-				<img class="w-347 h-589" src="<?= $card['url'] ?>" alt="<?= $card['alt'] ?>">
+				<img class="md:w-347 md:h-589 w-171 h-276 md:ml-0 ml-183 md:mt-0 mt--60" src="<?= $card['url'] ?>" alt="<?= $card['alt'] ?>">
 			<?php endif ?>
 			<?php $llave++; ?>
 		<?php endforeach; ?>
 	</section>
-	<section class="mb-250">
+	<section class="container md:mb-250 mb-90">
 		<?php foreach ($grilla as $key => $item) : ?>
 			<?php if ($key % 2 !== 0) : ?>
 				<div class="flex mb-130">
@@ -69,7 +76,7 @@ $imagen2 = get_field("imagen_adicional");
 			<?php endif; ?>
 		<?php endforeach; ?>
 	</section>
-	<section class=" flex justify-center w-full">
+	<section class="container flex justify-center w-full">
 		<div class="w-auto h-auto relative border-15 border-lightblack">
 			<img src="<?= $imagen2['url'] ?>" class="w-1095 h-395" alt="<?= $imagen2['url'] ?>">
 			<div class="absolute top-60 w-full flex items-start flex-col pl-30">
