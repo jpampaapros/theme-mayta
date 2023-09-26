b0a0933c8c836780d89afb031bf823ce7d903a68
JosCha94 <72150891+JosCha94@users.noreply.github.com>
2023-09-26 12:31:48 -0500

    carta mov

--- templates/carta.php
+++ templates/carta.php
@@ -14,40 +14,43 @@ $boton_carta = get_field("boton_texto_carta");
 $link_carta = get_field("boton_link_carta");
 
 ?>
-<main class="container h-auto py-150">
-	<section class="relative flex justify-center mb-150">
-		<div class="flex flex-col absolute top-350 left-320">
-			<h2 class="font-medium text-50 text-white"> <?= $titulo ?></h2>
+<main class="container h-auto md:py-150 py-50">
+	<section class="relative flex justify-center md:mb-150 mb-250">
+		<div class="flex flex-col absolute md:top-350 md:left-320 top-300 left--6">
+			<h2 class="font-medium md:text-50 text-35 text-white"> <?= $titulo ?></h2>
 			<div class="text-16 text-white"><?= $texto ?></div>
-			<a class="border border-white bg-transparent pt-20 pb-16 px-40 text-center h-auto w-250 text-18 font-medium my-20" href="<?= $link ?>"><?= $boton ?></a>
+			<a class="border border-white bg-transparent pt-20 pb-16 md:px-40 mx-20 text-center h-auto md:w-250 w-180 text-18 font-medium my-20" href="<?= $link ?>"><?= $boton ?></a>
 		</div>
-		<img class="w-713 h-629 ml-220" src="<?= $imagen['url'] ?> " alt="<?= $imagen['alt'] ?>">
+		<!-- <img class="w-713 h-629 ml-220" src="<?= $imagen['url'] ?> " alt="<?= $imagen['alt'] ?>"> -->
+		<?= render_image($imagen, "md:w-713 md:h-629 md:ml-220 w-307 h-335 ml-20") ?>
 	</section>
-	<section class="flex justify-center mb-150">
-		<?php
-		$switch = 1;
-		$cardCount = count($cards);
-		foreach ($cards as $item) : ?>
-			<?php if ($switch == $cardCount) : ?>
-				<div class="flex flex-col text-center p-40">
-					<h3 class="text-22 leading-33"><?= $item['titulo'] ?></h3>
-					<a href=""> VER MARIDAJE</a>
-				</div>
-			<?php else : ?>
-				<div class="flex flex-col text-center border-e border-white p-40">
-					<h3 class="text-22 leading-33"><?= $item['titulo'] ?></h3>
-					<a class="text-22" href="<?= $item['link'] ?>"> VER MARIDAJE</a>
-				</div>
-			<?php endif; ?>
-			<?php $switch++ ?>
-		<?php endforeach; ?>
+	<section class="hidden md:block">
+		<div class="flex justify-center mb-150">
+			<?php
+			$switch = 1;
+			$cardCount = count($cards);
+			foreach ($cards as $item) : ?>
+				<?php if ($switch == $cardCount) : ?>
+					<div class="flex flex-col text-center p-40">
+						<h3 class="text-22 leading-33"><?= $item['titulo'] ?></h3>
+						<a href=""> VER MARIDAJE</a>
+					</div>
+				<?php else : ?>
+					<div class="flex flex-col text-center border-e border-white p-40">
+						<h3 class="text-22 leading-33"><?= $item['titulo'] ?></h3>
+						<a class="text-22" href="<?= $item['link'] ?>"> VER MARIDAJE</a>
+					</div>
+				<?php endif; ?>
+				<?php $switch++ ?>
+			<?php endforeach; ?>
+		</div>
 	</section>
 	<section class="flex justify-center">
-<div class="flex flex-col items-center justify-center py-30 text-white">
-<?= $carta ?>
-<a class="pt-20 pb-16 px-40 text-18 bg-transparent border border-white my-20" href="<?= $link_carta ?>"><?= $boton_carta ?></a>
+		<div class="flex flex-col items-center justify-center py-30 text-white">
+			<?= $carta ?>
+			<a class="pt-20 pb-16 px-40 text-18 bg-transparent border border-white my-20" href="<?= $link_carta ?>"><?= $boton_carta ?></a>
 
-</div>
+		</div>
 	</section>
 
 </main>
