0a23b26467b79dd7243f1942b3e8252bd37ab937
JosCha94 <72150891+JosCha94@users.noreply.github.com>
2023-09-27 17:35:51 -0500

    cambios header

--- header.php
+++ header.php
@@ -39,13 +39,9 @@ $urlActual = $_SERVER['REQUEST_URI'];
         ));
         ?>
       </div>
-      <a href="#" class="w-20 h-20 md:block hidden"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0,0,256,256" width="20px" height="20px">
-          <g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
-            <g transform="scale(5.12,5.12)">
-              <path d="M16,3c-7.16752,0 -13,5.83248 -13,13v18c0,7.16752 5.83248,13 13,13h18c7.16752,0 13,-5.83248 13,-13v-18c0,-7.16752 -5.83248,-13 -13,-13zM16,5h18c6.08648,0 11,4.91352 11,11v18c0,6.08648 -4.91352,11 -11,11h-18c-6.08648,0 -11,-4.91352 -11,-11v-18c0,-6.08648 4.91352,-11 11,-11zM37,11c-1.10457,0 -2,0.89543 -2,2c0,1.10457 0.89543,2 2,2c1.10457,0 2,-0.89543 2,-2c0,-1.10457 -0.89543,-2 -2,-2zM25,14c-6.06329,0 -11,4.93671 -11,11c0,6.06329 4.93671,11 11,11c6.06329,0 11,-4.93671 11,-11c0,-6.06329 -4.93671,-11 -11,-11zM25,16c4.98241,0 9,4.01759 9,9c0,4.98241 -4.01759,9 -9,9c-4.98241,0 -9,-4.01759 -9,-9c0,-4.98241 4.01759,-9 9,-9z"></path>
-            </g>
-          </g>
-        </svg></a>
+      <a href="<?= $link_red ?>" target="_blank" rel="nooponer" class="w-20 h-20 md:block hidden">
+      <?= $icono_red ?>
+      </a>
       <div class="h-30 w-1 bg-white ml-10 mr-0 md:block hidden"></div>
       <div class="w-auto h-50">
         <select name="idioma" id="idioma" class="bg-transparent border-none text-white text-18">
@@ -126,7 +122,7 @@ $urlActual = $_SERVER['REQUEST_URI'];
         </a>
       <?php endif; ?>
       <?php if ($link_red) : ?>
-        <a href="<?= $link_red ?>" class="w-20 h-20 block md:hidden">
+        <a href="<?= $link_red ?>" target="_blank" rel="nooponer" class="w-20 h-20 block md:hidden">
           <?= $icono_red ?>
         </a>
       <?php endif; ?>
