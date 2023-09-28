73605f0aac9f07cc397ecc148042d2bbbd41e60f
JosCha94 <72150891+JosCha94@users.noreply.github.com>
2023-09-28 18:23:27 -0500

    arreglos header

--- header.php
+++ header.php
@@ -27,7 +27,7 @@ $urlActual = get_permalink();
 ?>
 <body class="<?php echo $body_classes;?> " <?php generate_do_microdata('body'); ?> style="background-image: url(<?= home_url();?>/wp-content/uploads/2023/09/fondo.webp)">
   <div class="" data-scroll-container>
-  <header class="flex justify-between container md:py-36 items-baseline bg-transparent py-20">
+  <header class="flex justify-between container md:py-36 bg-transparent py-20">
     <a href="<?= home_url() ?>" class="hidden md:block">
       <?= render_image($logo_desktop, "h-67 w-auto hidden md:block"); ?>
     </a>
