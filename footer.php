d920041e0de5bd0ff37140ec5ec923942f78a811
JosCha94 <72150891+JosCha94@users.noreply.github.com>
2023-09-28 11:37:16 -0500

    cambio

--- footer.php
+++ footer.php
@@ -85,11 +85,14 @@ $urlActual = get_permalink();
 		<?php if ($iconos) : ?>
 			<?= render_image($iconos, "absolute top-200 right-200 w-60 h-auto md:block hidden"); ?>
 		<?php endif; ?>
+		<?php endif; ?>
 		<div class="flex justify-center mb-70 md:mb-89">
 			<a href="<?= $link_btn ?>" style="background-image:url(<?= home_url(); ?>/wp-content/uploads/2023/09/btn-reserve.jpg)" class="w-260 h-60 text-black flex items-center justify-center text-18 pt-20 pb-16 px-40 font-medium border-solid border-1 border-white uppercase bg-cover">
 				<?= $text_btn ?>
 			</a>
 		</div>
+
+	<?php if ($urlActual != home_url('/')) : ?>
 		<div class="hidden md:flex justify-center uppercase footer__menu mb-41">
 			<?php
 			wp_nav_menu(array(
@@ -135,4 +138,5 @@ wp_footer();
 ?>
 </body>
 </div>
+
 </html>
