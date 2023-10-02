9d81c6bfdbcc4728146ff9501fe11b0f1292a4f1
JosCha94 <72150891+JosCha94@users.noreply.github.com>
2023-10-02 15:58:48 -0500

    correccion

--- src/partials/header/index.js
+++ src/partials/header/index.js
@@ -1,4 +1,4 @@
-import $, { each } from "jquery";
+import $ from "jquery";
 $(function () {
   $(".hamburger").click(function () {
     $("#slidemenu").removeClass("hidden").addClass("block").animate({ left: 0 }, 800);
@@ -14,25 +14,6 @@ $(function () {
 
 // let dominio = window.location.hostname;
 let rutaActual = window.location.pathname;
-// $('#idioma').change(function () {
-
-//   let opcionSeleccionada = $(this).val();
-
-
-//   if (opcionSeleccionada === "es") {
-//     let nuevaRuta = rutaActual.replace('/en', '');
-//     let dd = window.location = "http://" + dominio + nuevaRuta;
-//   } else if (opcionSeleccionada === "en") {
-//     window.location = "http://" + dominio + "/en" + rutaActual;
-//   }
-
-// if (opcionSeleccionada === "es") {
-//   let nuevaRuta = rutaActual.replace('/en', '');
-//   let dd = window.location = "http://" + dominio + "/mayta" + nuevaRuta;
-// } else if (opcionSeleccionada === "en") {
-//   window.location = "http://" + dominio + "/mayta/en" + rutaActual;
-// }
-// });
 
 if (rutaActual.indexOf("/en/") !== -1) {
   let ul = $(".idiomas");
