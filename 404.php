58d60d2c23ace3e7c6e7ebf0c3c03f050c46d80b
Joses-Apros <jchavez@apros.com.pe>
2023-10-16 11:57:17 -0500

    pag 404 change

--- 404.php
+++ 404.php
@@ -3,10 +3,13 @@
   //Template name: Home
   get_header();
   ?>
-  <main class="w-full">
+  <main class="w-full py-80 ">
+<div class="pag-404 flex justify-center items-center container flex-col h-auto">
 
-        <h1 class="flex justify-center items-center min-h-200 text-50 leading-50">404</h1>
-
+        <h1 class=" text-120 leading-50">404</h1>
+<p class="text-30 mt-20">Pagina no encontrada</p>
+        <a href="<?= home_url() ?>" class="text-white hover:text-blue mt-20">Regresar al Home</a>
+</div>
   </main>
 
   <?php
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,1 +1,8 @@
 import $ from "jquery";
+
+
+// PAGINA 404
+if (window.location.href.indexOf("/en/") > -1) {
+    $('.pag-404 p').text('Page not found');
+    $('.pag-404 a').text('Return to home');
+}
