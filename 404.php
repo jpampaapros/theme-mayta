c8162183d30bc351f7da0b299247a280e1cfb8de
Joses-Apros <jchavez@apros.com.pe>
2023-10-16 11:57:54 -0500

    pag 404 change

--- 404.php
+++ 404.php
@@ -1,17 +1,16 @@
+<?php
+//Template name: Home
+get_header();
+?>
+<main class="w-full py-80 ">
+      <div class="pag-404 flex justify-center items-center container flex-col h-auto">
+
+            <h1 class=" text-120 leading-50">404</h1>
+            <p class="text-30 mt-20">Pagina no encontrada</p>
+            <a href="<?= home_url() ?>" class="text-white hover:text-blue mt-20">Regresar al Home</a>
+      </div>
+</main>
 
 <?php
-  //Template name: Home
-  get_header();
-  ?>
-  <main class="w-full py-80 ">
-<div class="pag-404 flex justify-center items-center container flex-col h-auto">
-
-        <h1 class=" text-120 leading-50">404</h1>
-<p class="text-30 mt-20">Pagina no encontrada</p>
-        <a href="<?= home_url() ?>" class="text-white hover:text-blue mt-20">Regresar al Home</a>
-</div>
-  </main>
-
-  <?php
-  get_footer();
-  ?>
+get_footer();
+?>
