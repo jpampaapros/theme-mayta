26ccd5f3ab703e86f2999d7f2cce65e355090656
Joses-Apros <joses.acp@gmail.com>
2024-01-03 12:53:09 -0500

    cambio en carta

--- src/pages/carta/index.js
+++ src/pages/carta/index.js
@@ -1,16 +1,17 @@
 import $ from "jquery";
 
-$('.maridaje').mouseover(function(event) {
-    let dataId = $(this).data('id'); 
+$('.maridaje').mouseover(function (event) {
+    let dataId = $(this).data('id');
     $('.price' + dataId).css({
         'opacity': '0.9',
         'transition': 'opacity 0.5s'
     });
-}).mouseout(function(event) {
-    let dataId = $(this).data('id'); 
+}).mouseout(function (event) {
+    let dataId = $(this).data('id');
     $('.price' + dataId).css({
         'opacity': '0',
         'transition': 'opacity 0.5s'
     });
 });
+
 
